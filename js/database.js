95c3ded213ba9beffd2ff2d192098df3fe4d4fe2
Jim Geisler <jim.geisler@gmail.com>
2011-03-08 22:30:02 -0500

    Included the mongodb lib in the project. Updated the database package to use the library.

--- js/database.js
+++ js/database.js
@@ -1,13 +1,13 @@
 var sys = require('sys'),
-    Db = require('mongodb').Db,
-    Connection = require('mongodb').Connection,
-    Server = require('mongodb').Server,
-    BSON = require('mongodb').BSONNative;
+    Db = require('../lib/mongodb').Db,
+    Connection = require('../lib/mongodb').Connection,
+    Server = require('../lib/mongodb').Server,
+    BSON = require('../lib/mongodb').BSONNative;
 
 
 var host = process.env['MONGO_NODE_DRIVER_HOST'] != null ? process.env['MONGO_NODE_DRIVER_HOST'] : 'localhost';
 var port = process.env['MONGO_NODE_DRIVER_PORT'] != null ? process.env['MONGO_NODE_DRIVER_PORT'] : Connection.DEFAULT_PORT;
 
-var db = new Db('dynamic-node-server-mongo', new Server(host, port, {}), {native_parser:true});
+var db = new Db('dynamic-node-server-mongo', new Server(host, port, {}), {native_parser:false});
 
 exports.db = db;
